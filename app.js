3a7ff6bdf7290e018b1968ab2686c7ec75a896ae
Gani <abdulgani.work@gmail.com>
2022-10-01 02:02:34 +0530

    initial commit - added nav+hero+about+experience

---
+++ app.js
@@ -0,0 +1,17 @@
+//jshint esversion:6
+
+const express = require("express");
+const app = express();
+const port = 5000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(port, () => {
+  console.log(
+    `Example app listening on port ${port}\nhttp://localhost:${port}/`
+  );
+});
